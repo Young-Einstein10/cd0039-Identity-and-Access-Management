736782f1de6f6193cf05be103f6d985e8ac71aa0
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2022-07-25 01:39:09 +0100

    feature: complete project requirements

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import os
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
@@ -9,7 +10,28 @@ from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
+
+
+"""
+    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+"""
 CORS(app)
+
+"""
+    @TODO: Use the after_request decorator to set Access-Control-Allow
+"""
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add(
+        "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+    )
+    response.headers.add(
+        "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+    )
+    return response
+
 
 '''
 @TODO uncomment the following line to initialize the datbase
@@ -30,6 +52,18 @@ CORS(app)
 '''
 
 
+@app.route("/drinks")
+def fetch_short_drinks():
+    drinks = Drink.query.order_by(Drink.id).all()
+
+    short_drinks = [drink.short() for drink in drinks]
+
+    return jsonify({
+        'success': True,
+        'drinks': short_drinks,
+    })
+
+
 '''
 @TODO implement endpoint
     GET /drinks-detail
@@ -38,6 +72,19 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route("/drinks-detail")
+@requires_auth('get:drinks-detail')
+def fetch_long_drinks(params):
+    drinks = Drink.query.order_by(Drink.id).all()
+
+    long_drinks = [drink.long() for drink in drinks]
+
+    return jsonify({
+        'success': True,
+        'drinks': long_drinks,
+    })
 
 
 '''
@@ -49,6 +96,26 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route("/drinks", methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(params):
+    body = request.get_json()
+
+    new_title = body.get("title", None)
+    new_recipe = json.dumps(body.get("recipe", None))
+
+    drink = Drink(title=new_title, recipe=new_recipe)
+
+    drink.insert()
+
+    return jsonify(
+        {
+            'success': True,
+            'drinks': [drink.long()]
+        }
+    )
 
 
 '''
@@ -64,6 +131,32 @@ CORS(app)
 '''
 
 
+@app.route("/drinks/<id>", methods=['PATCH'])
+@requires_auth('patch:drinks')
+def edit_drink(params, id):
+    drink = Drink.query.filter(
+        Drink.id == id).one_or_none()
+
+    if drink is None:
+        abort(404)
+
+    body = request.get_json()
+
+    incoming_title = body.get("title", None)
+    incoming_recipe = json.dumps(body.get("recipe", None))
+
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    drink.title = incoming_title
+    drink.recipe = incoming_recipe
+
+    drink.update()
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long()]
+    })
+
+
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -76,19 +169,29 @@ CORS(app)
 '''
 
 
+@app.route("/drinks/<id>", methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(params, id):
+    drink = Drink.query.filter(
+        Drink.id == id).one_or_none()
+
+    if drink is None:
+        abort(404)
+
+    drink.delete()
+
+    return jsonify(
+        {
+            "success": True,
+            "delete": id,
+        }
+    )
+
+
 # Error Handling
 '''
 Example error handling for unprocessable entity
 '''
-
-
-@app.errorhandler(422)
-def unprocessable(error):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
 
 
 '''
@@ -102,13 +205,64 @@ def unprocessable(error):
 
 '''
 
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return (jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422)
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return (jsonify({"success": False, "error": 400, "message": "bad request"}), 400)
+
+
+@app.errorhandler(403)
+def bad_request(error):
+    return (jsonify({"success": False, "error": 403, "message": "Forbidden"}), 403)
+
+
+@app.errorhandler(500)
+def internal_server(error):
+    return (jsonify({"success": False, "error": 500, "message": "internal server error"}), 500)
+
+
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return (
+        jsonify({"success": False, "error": 404,
+                 "message": "resource not found"}),
+        404,
+    )
+
+
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+@app.errorhandler(401)
+def not_found(error):
+    return (
+        jsonify({"success": False, "error": 401,
+                 "message": "not authorized"}),
+        401,
+    )
+
+
+@app.errorhandler(AuthError)
+def process_AuthError(error):
+    response = jsonify(error.error)
+    response.status_code = error.status_code
+
+    return response
--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: "dev-0fcsgqgl.us", // the auth0 domain prefix
+    audience: "coffee-auth-api", // the audience set for the auth0 app
+    clientId: "1HKNhH76kypqsXt5NOoWH0N68I8jV5WX", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:4200", // the base url of the running ionic application.
+  },
 };
